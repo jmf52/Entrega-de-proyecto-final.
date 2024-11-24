30068464a85b829dfd52c1a981a31dfb8ef62c75
Martínez Julio <23302108@utfv.edu.mx>
2024-11-23 20:48:22 -0600

    Update history.js
    Signed-off-by: Martínez Julio <23302108@utfv.edu.mx>

--- history.js
+++ history.js
@@ -4,7 +4,6 @@ const backButton = document.getElementById('backButton');
 
 const storedImages = JSON.parse(localStorage.getItem('imageHistory')) || [];
 
-
 function renderHistory() {
     historyContainer.innerHTML = '';
     storedImages.forEach((image, index) => {
@@ -15,21 +14,14 @@ function renderHistory() {
         imgElement.src = image;
         imgElement.alt = `Imagen ${index + 1}`;
 
-  
+     
         const deleteButton = document.createElement('button');
         deleteButton.className = 'delete-btn';
         deleteButton.textContent = 'Eliminar';
         deleteButton.onclick = () => deleteImage(index);
 
-        
-        const downloadButton = document.createElement('button');
-        downloadButton.className = 'download-btn';
-        downloadButton.textContent = 'Descargar';
-        downloadButton.onclick = () => downloadImage(image);
-
         imageCard.appendChild(imgElement);
         imageCard.appendChild(deleteButton);
-        imageCard.appendChild(downloadButton);
 
         historyContainer.appendChild(imageCard);
     });
@@ -43,20 +35,9 @@ function deleteImage(index) {
 }
 
 
-function downloadImage(imageUrl) {
-    const link = document.createElement('a');
-    link.href = imageUrl;
-    link.download = 'imagen-favorita.jpg';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
-
-
 backButton.onclick = () => {
     window.location.href = 'index.html';
 };
 
 
 renderHistory();
-
